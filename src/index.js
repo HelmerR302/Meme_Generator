9e1406a618a88c39cd2d3d367f144e15d119a1bc
Helmer W <helmer.z@yahoo.com>
2023-03-23 19:22:40 +0000

    deploy setting

--- src/index.js
+++ src/index.js
@@ -6,9 +6,12 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename="/Meme_Generator">
+    <HashRouter basename="">
       <App />
     </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+//<HashRouter basename="/Meme_Generator">
+// https://helmerr302.github.io/Meme_Generator/Meme_Generator/static/js/main.4cf94e97.js
