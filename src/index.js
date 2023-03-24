82fe7fb1c3e6fc39b6cca3369366b7f200defa58
Helmer W <helmer.z@yahoo.com>
2023-03-24 20:40:37 +0000

    cleanup after deploy

--- src/index.js
+++ src/index.js
@@ -5,28 +5,3 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
-//const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<App />);
-
-// github deploy attemp:
-//  "homepage": "https://helmerr302.github.io/Meme_Generator/",
-//   "homepage": "https://github.com/HelmerR302/",
-// import { HashRouter } from "react-router-dom";
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <HashRouter basename="">
-//       <App />
-//     </HashRouter>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-// "predeploy": "npm run build",
-// "deploy": "gh-pages -d build",
